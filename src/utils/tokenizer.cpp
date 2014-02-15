2e9fbf3e7c9d4e10bcd90b5ea9420ca59c427cc5
Juan Ángel Moreno Fernández <jamf.dev@gmail.com>
2014-02-15 20:16:58 +0100

    Simplification of Tokenizer class
    Tokenizer class has been simplified, now it only divides strings
    in words without parsing properties.

--- src/utils/tokenizer.cpp
+++ src/utils/tokenizer.cpp
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
  * gelide
- * Copyright (C) 2008 - 2011 Juan Ángel Moreno Fernández
+ * Copyright (C) 2008 - 2014 Juan Ángel Moreno Fernández
  *
  * gelide is free software.
  *
@@ -19,401 +19,187 @@
  * along with gelide.  If not, see <http://www.gnu.org/licenses/>
  */
 
+#include "tokenizer.hpp"
 #include <fstream>
-#include <stdlib.h>
+/*#include <stdlib.h>
 #include <string.h>
 #include <algorithm>
-#include "tokenizer.hpp"
+*/
 
-#define TK_DEFAULT_DELIMITERS	" \n\r\t"
-#define TK_DEFAULT_DELIMITERS_SIZE	4
 
-CTokenizer::CToken::CToken(const int p_type, const unsigned int p_value, char* p_string){
-	m_type = p_type;
-	m_value = p_value;
-	m_string = p_string;
-}
-
-int CTokenizer::CToken::getType(void){
-	return m_type;
-}
-
-unsigned int CTokenizer::CToken::getValue(void){
-	return m_value;
-}
-
-char* CTokenizer::CToken::getString(void){
-	return m_string;
-}
-
-void CTokenizer::CToken::setType(const int p_type){
-	m_type = p_type;
-}
-
-void CTokenizer::CToken::setValue(const unsigned int p_value){
-	m_value = p_value;
-}
-
-void CTokenizer::CToken::setString(char* p_string){
-	static char l_zero[] = "";
-
-	if(!p_string)
-		m_string = l_zero;
-	else
-		m_string = p_string;
-}
-
-/*---------------------------------------------------------------------------*/
-
-CTokenizer::CTokenizer(void){
-	// Por defecto detección de cadenas y números activada
-	m_detect_strings = true;
-	m_detect_numbers = true;
-	m_words = NULL;
-	m_words_count = 0;
-	m_delimiters = NULL;
-	m_delimiters_count = 0;
-	m_buff = NULL;
-	m_buff_pos = NULL;
-	m_init = false;
-	// Establecemos los delimitadores por defecto
-	setDelimiters((char *)TK_DEFAULT_DELIMITERS, TK_DEFAULT_DELIMITERS_SIZE);
-}
-
-CTokenizer::~CTokenizer(){
-	if(m_buff)
-		delete[] m_buff;
-	if(m_delimiters)
-		delete[] m_delimiters;
-	if(m_words)
-		delete[] m_words;
-}
-
-bool CTokenizer::initFromFile(const Glib::ustring& p_file){
-	std::ifstream m_file;
-	int l_fsize;
-	int l_readed;
-
-	// Nos aseguramos de liberar la memoria y hacer nulos los punteros
-	if(m_buff)
-		delete[] m_buff;
-	m_buff = NULL;
-	m_buff_pos = NULL;
-	m_init = false;
-
-	m_file.open(p_file.data());
-	// Comprobamos si la apertura fue correcta
-	if(!m_file.good()){
-		m_file.close();
-		return false;
-	}
-	// Obtenemos el tamaño del fichero
-	m_file.seekg (0, std::ios::end);
-	l_fsize = m_file.tellg();
-	m_file.seekg (0, std::ios::beg);
-	// Reservamos la memoria necesaria
-	m_buff = new char[l_fsize + 1];
-	if(!m_buff){
-		m_file.close();
-		return false;
-	}
-	// Cargamos el contenido completo del fichero en memoria
-	m_file.read (m_buff, l_fsize);
-	l_readed = m_file.gcount();
-	m_file.close();
-	// Reiniciamos el puntero de lectura
-	m_buff_pos = m_buff;
-	m_buff[l_readed] = '\0';
-	m_init = true;
-	findNextToken();
-	return true;
-}
-
-bool CTokenizer::initFromString(const Glib::ustring& p_str){
-	std::ifstream m_file;
-	int l_ssize;
-	int l_readed;
-
-	// Nos aseguramos de liberar la memoria y hacer nulos los punteros
-	if(m_buff)
-		delete[] m_buff;
-	m_buff = NULL;
-	m_buff_pos = NULL;
-	m_init = false;
-
-	// Obtenemos el tamaño de la cadena
-	l_ssize = p_str.size();
-	// Reservamos la memoria necesaria
-	m_buff = new char[l_ssize + 1];
-	if(!m_buff){
-		m_file.close();
-		return false;
-	}
-	// Cargamos el contenido completo de la cadena en el buffer
-	l_readed = p_str.copy(m_buff, l_ssize, 0);
-	// Reiniciamos el puntero de lectura
-	m_buff_pos = m_buff;
-	m_buff[l_readed] = '\0';
-	m_init = true;
-	findNextToken();
-	return true;
-}
-
-bool CTokenizer::initFromMemory(const char *p_buffer, int p_size){
-	int l_ssize;
-
-	// Nos aseguramos de liberar la memoria y hacer nulos los punteros
-	if(m_buff)
-		delete[] m_buff;
-	m_buff = NULL;
-	m_buff_pos = NULL;
-	m_init = false;
-
-	// Reservamos la memoria necesaria
-	m_buff = new char[p_size + 1];
-	if(!m_buff)
-		return false;
-
-	// Copiamos el contenido del buffer
-	memcpy(m_buff, p_buffer, p_size);
-
-	// Reiniciamos el puntero de lectura
-	m_buff_pos = m_buff;
-	m_buff[p_size] = '\0';
-	m_init = true;
-	findNextToken();
-	return true;
-}
-
-void CTokenizer::setDelimiters(char* p_delimiters, unsigned int p_count){
-	// Si no se especifican delimitadores, asignamos los internos
-	if(!p_delimiters){
-		p_delimiters = (char *) TK_DEFAULT_DELIMITERS;
-		p_count = (unsigned int) TK_DEFAULT_DELIMITERS_SIZE;
-	}
-	// Limpiamos la memoria de los anteriores delimitadores e iniciamos contador
-	delete[] m_delimiters;
-	m_delimiters = NULL;
-	m_delimiters_count = 0;
-
-	// Solicitamos memoria para copiar los delimitadores
-	m_delimiters = new char[p_count];
-	if(m_delimiters){
-		strncpy(m_delimiters, p_delimiters, p_count);
-		m_delimiters_count = p_count;
-	}
-}
-
-void CTokenizer::setReservedWords(TReservedWord p_words[], unsigned int p_count){
-	unsigned int l_ind;
-
-	// Limpiamos la memoria de las anteriores palabras e iniciamos el contador
-	if(m_words)
-		delete[] m_words;
-	m_words = NULL;
-	m_words_count = 0;
-
-	// Solicitamos memoria para las nuevas palabras reservadas
-	m_words = new TWord[p_count];
-	if(m_words){
-		m_words_count = p_count;
-		// Copiamos las palabras reservadas pasadas
-		for(l_ind = 0; l_ind < p_count; l_ind++){
-			m_words[l_ind].m_type = p_words[l_ind].m_type;
-			m_words[l_ind].m_size = strlen(p_words[l_ind].m_name);
-			strcpy(m_words[l_ind].m_name, p_words[l_ind].m_name);
-		}
-	}
-	std::sort(m_words, m_words + p_count, CTokenizer::compareWords);
-}
-
-void CTokenizer::setStringsDetection(const bool p_detect){
-	m_detect_strings = p_detect;
-}
-
-void CTokenizer::setNumbersDetection(const bool p_detect){
-	m_detect_numbers = p_detect;
-}
-
-void CTokenizer::nextToken(CToken& p_token){
-	if(!m_init)
-		return;
-	// Valor de control para token
-	p_token.m_type = TK_NOTK;
-	p_token.m_value = -1;
-	p_token.m_string = NULL;
-	while(p_token.m_type == TK_NOTK){
-		switch(*m_buff_pos){
-		// Fin de fichero
-		case '\0':
-			p_token.m_type = TK_EOF;
-			p_token.m_value = 0;
-			// Hacemos que apunte a "/0", para evitar segfaults
-			p_token.m_string = m_buff_pos;
-			break;
-		// Comienzo de una cadena de caracteres
-		case '"':
-			if(m_detect_strings){
-				// Obtenemos la cadena
-				p_token.m_type = TK_STR;
-				p_token.m_value = 0;
-				p_token.m_string = this->getString();
-				break;
-			}
-		default:
-			// Llegados a este punto, como minimo será un identificador
-			p_token.m_type = TK_ID;
-			p_token.m_string = this->getIdentifier();
-
-			// Comprobamos si se trata de un número
-			if(m_detect_numbers){
-				if(checkNum(p_token))
-					break;
-			}
-
-			checkReservedWords(p_token);
-		}
-		// Posicionamos al comienzo del siguiente token
-		findNextToken();
-	}
-}
-
-bool CTokenizer::hasMoreTokens(void){
-	return *m_buff_pos != '\0';
-}
-
-void CTokenizer::reset(){
-	// Nos aseguramos de liberar la memoria y hacer nulos los punteros
-	if(m_buff)
-		delete[] m_buff;
-	m_buff = NULL;
-	m_buff_pos = NULL;
-
-	m_init = false;
-}
-
-bool CTokenizer::isDelimiter(void){
-	unsigned int l_ind = 0;
-
-	// Buscamos nuestra cadena hasta agotar todos los separadores
-	while((l_ind < m_delimiters_count) && (*m_buff_pos != m_delimiters[l_ind]))
-		l_ind++;
-	// Comprobamos si se encontró alguno
-	if(l_ind < m_delimiters_count)
-		return true;
-	return false;
-}
-
-bool CTokenizer::checkReservedWords(CToken& p_token){
-	int l_pos;
-
-	l_pos = binarySearchWord(p_token.m_string);
-	if(l_pos != -1){
-		p_token.m_type = m_words[l_pos].m_type;
-		p_token.m_string = m_words[l_pos].m_name;
-		return true;
-	}
-	return false;
-}
-
-bool CTokenizer::checkNum(CToken& p_token){
-	char* l_string = NULL;
-	char* l_ind = NULL;
-
-	l_string = p_token.m_string;
-	l_ind = l_string;
-
-	// Avanzamos mientras tengamos números
-	while(isdigit((int) *l_ind))
-		l_ind++;
-	// Si no se avanza, no tenemos ningún número
-	if(l_ind == l_string)
-		return false;
-	if(*l_ind == '\0'){
-		p_token.m_type = TK_NUM;
-		p_token.m_value = atoi(l_string);
-		return true;
-	}
-	return false;
-}
-
-bool CTokenizer::findNextToken(void){
-	while(isDelimiter())
-		m_buff_pos++;
-	if(*m_buff_pos == '\0')
-		return false;
-	else
-		return true;
-}
-
-char* CTokenizer::getString(void){
-	char* l_string;
-
-	// Saltamos las comillas iniciales y colocamos el inicio de cadena
-	m_buff_pos++;
-	l_string = m_buff_pos;
-
-	// Ignoramos caracteres escapados iniciales
-	while(*m_buff_pos == '\\')
-		m_buff_pos += 2;
-
-	// Consideramos que la cadena finaliza en " o final de línea
-	while( (*m_buff_pos != '"') && (*m_buff_pos != '\0') ){
-		m_buff_pos++;
-		// Ignoramos caracteres escapados
-		while(*m_buff_pos == '\\')
-			m_buff_pos += 2;
-
-	}
-
-	// Si no llegamos a eof, marcamos el fin de la cadena y avanzamos
-	// para evitar un eof erroneo en la siguiente lectura
-	if(*m_buff_pos == '"'){
-		*m_buff_pos = '\0';
-		m_buff_pos++;
-	}
-
-	return l_string;
-}
-
-char* CTokenizer::getIdentifier(void){
-	char* l_string;
-	// Asignamos el puntero de la cadena en el comienzo del identificador
-	l_string = m_buff_pos;
-	while((!isDelimiter()) && (*m_buff_pos != '\0'))
-		m_buff_pos++;
-
-	// Si no llegamos a eof, marcamos el fin de la cadena y avanzamos
-	// para evitar un eof erroneo en la siguiente lectura
-	if(*m_buff_pos != '\0'){
-		*m_buff_pos = '\0';
-		m_buff_pos++;
-	}
-	return l_string;
-}
-
-bool CTokenizer::compareWords(const TWord& p_word0, const TWord& p_word1){
-	return (strcmp(p_word0.m_name, p_word1.m_name) < 0);
-}
-
-int CTokenizer::binarySearchWord(char* p_key)
+Tokenizer::Tokenizer(void):
+	m_delimiters(DEFAULT_DELIMITERS),
+	m_last_delimiter('\0'),
+	m_detect_strings(true)
 {
-	int l_top = m_words_count-1;
-	int l_bottom = 0;
-	int l_center;
-	int l_str_cmp;
+	m_buff_pos = m_buff.begin();
+}
 
-	while (l_bottom <= l_top){
-		l_center = (l_top + l_bottom)/2;
-		l_str_cmp = strcmp(m_words[l_center].m_name, p_key);
-		if(l_str_cmp == 0)
-			return l_center;
-		else{
-			if(l_str_cmp > 0)
-				l_top = l_center - 1;
-			else
-				l_bottom = l_center + 1;
-			}
+Tokenizer::~Tokenizer()
+{
+}
+
+bool Tokenizer::initFromFile(const std::string& file)
+{
+	std::ifstream file_stream;
+	unsigned int size;
+
+	file_stream.open(file.data());
+	if (!file_stream.good())
+	{
+		file_stream.close();
+		return false;
 	}
-	return -1;
+	// Obtenemos el tamaño del fichero para redimensionar la cadena
+	file_stream.seekg (0, std::ios::end);
+	size = file_stream.tellg();
+	file_stream.seekg (0, std::ios::beg);
+	m_buff.resize(size);
+	// Cargamos el contenido completo del fichero en la cadena
+	file_stream.read(&m_buff[0], size);
+	file_stream.close();
+	// Reiniciamos el puntero de lectura y los datos internos
+	m_buff_pos = m_buff.begin();
+	m_token.clear();
+	m_last_delimiter = '\0';
+
+	return true;
+}
+
+bool Tokenizer::initFromString(const std::string& str)
+{
+	m_buff = str;
+	m_buff_pos = m_buff.begin();
+	m_token.clear();
+	m_last_delimiter = '\0';
+
+	return true;
+}
+
+bool Tokenizer::initFromMemory(const char *buffer, const unsigned int size)
+{
+	m_buff.resize(size);
+	m_buff.assign(buffer, size);
+	m_buff_pos = m_buff.begin();
+	m_token.clear();
+	m_last_delimiter = '\0';
+
+	return true;
+}
+
+void Tokenizer::setDelimiters(const std::string& delimiters)
+{
+	m_delimiters = delimiters;
+}
+
+void Tokenizer::setStringDetection(const bool detect)
+{
+	m_detect_strings = detect;
+}
+
+std::string Tokenizer::nextToken(void)
+{
+	m_token.clear();
+
+	if (hasMoreTokens())
+	{
+		// Comprobamos si estamos en una cadena y hay que extraerla
+		if ((*m_buff_pos == '"') && m_detect_strings)
+		{
+			m_token = getString();
+			return m_token;
+		}
+		while ((m_buff_pos != m_buff.end()) && !isDelimiter())
+		{
+			m_token += *m_buff_pos;
+			++m_buff_pos;
+		}
+	}
+	return m_token;
+}
+
+bool Tokenizer::hasMoreTokens(void)
+{
+	// Saltamos delimitadores iniciales y guardamos el último delimitador
+	while ((m_buff_pos != m_buff.end()) && isDelimiter())
+	{
+		m_last_delimiter = *m_buff_pos;
+		++m_buff_pos;
+	}
+	if (m_buff_pos != m_buff.end())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+char Tokenizer::lastDelimiter(void)
+{
+	return m_last_delimiter;
+}
+
+void Tokenizer::reset(void)
+{
+	m_buff_pos = m_buff.begin();
+	m_token.clear();
+	m_last_delimiter = '\0';
+}
+
+std::vector<std::string> Tokenizer::split(void)
+{
+	std::vector<std::string> tokens;
+
+	while (hasMoreTokens())
+	{
+		tokens.push_back(nextToken());
+	}
+
+	return tokens;
+}
+
+bool Tokenizer::isDelimiter(void)
+{
+	return(m_delimiters.find(*m_buff_pos) != std::string::npos);
+}
+
+std::string Tokenizer::getString(void)
+{
+
+	std::string str;
+
+	// Saltamos las comillas iniciales donde hemos sido llamados
+	++m_buff_pos;
+
+	// Comprobamos caracteres escapados iniciales
+	while (*m_buff_pos == '\\')
+	{
+		str += *m_buff_pos;
+		str += *(m_buff_pos + 1);
+		m_buff_pos += 2;
+	}
+
+	// Consideramos que la cadena finaliza en " o al final del buffer
+	while ((*m_buff_pos != '"') && (m_buff_pos != m_buff.end()) )
+	{
+		str += *m_buff_pos;
+		++m_buff_pos;
+		// Caracteres escapados internos
+		while (*m_buff_pos == '\\')
+		{
+			str += *m_buff_pos;
+			str += *(m_buff_pos + 1);
+			m_buff_pos += 2;
+		}
+	}
+
+	// Si paramos en unas comillas, pasamos a la siguiente posición para evitar
+	// que se vuelva a interpretar la posición como cadena
+	if (*m_buff_pos == '"')
+	{
+		++m_buff_pos;
+	}
+	return str;
 }
--- src/utils/tokenizer.hpp
+++ src/utils/tokenizer.hpp
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
  * gelide
- * Copyright (C) 2008 - 2011 Juan Ángel Moreno Fernández
+ * Copyright (C) 2008 - 2014 Juan Ángel Moreno Fernández
  *
  * gelide is free software.
  *
@@ -22,264 +22,138 @@
 #ifndef _TOKENIZER_HPP_
 #define _TOKENIZER_HPP_
 
-#include "../gelide.hpp"
-#include <glibmm/ustring.h>
+#include <string>
+#include <vector>
 
-#define TOKENIZER_RES_WORD_SIZE	25	/**< Tamaño máximo de las palabras reservadas */
+
+// Delimitadores por defecto: Espacio, Salto de línea, Retorno de carro,
+// Tabulador horizontal, Tabulador vertical y Avance de página
+static const std::string DEFAULT_DELIMITERS = " \n\r\t\v\f";
 
 /**
- * Clase encargada del particionamiento de de un fichero de texto en bloques
- * fundamentales (tokens) de un conjunto dado como parámetro y teniendo en
- * cuenta unos delimitadores determinados.
+ * Tokenizador de cadenas simple.
+ *
+ * Permite trocear cadenas de texto o el contenido de un fichero en sus
+ * sub-bloques.
+ * Permite establecer los caracteres que se detectarán como delimitadores. Por
+ * defecto detecta como delimitadores los caracteres de Espacio, Salto de línea,
+ * Retorno de carro, Tabulador horizontal, Tabulador vertical y Avance de
+ * página.
+ * Incluye soporte para detectar cadenas de texto entrecomilladas activado por
+ * defecto.
  */
-class CTokenizer
+class Tokenizer
 {
 public:
-
-	/**
-	 * Tokens genericos devueltos por el tokenizador. Se definen negativos, para
-	 * utilizar los positivos con las palabras reservadas del usuario.
-	 */
-	enum TTokenTypes{
-		TK_NOTK	= -1,		/**< Token Notoken */
-		TK_EOF = -2,		/**< Token final de fichero */
-		TK_UNK = -3,		/**< Token desconocido */
-		TK_ID = -4,			/**< Token identificador */
-		TK_NUM = -5,		/**< Token número */
-		TK_STR = -6			/**< Token cadena */
-	};
-
-	/** Tipo para especificar las palabras reservadas */
-	typedef struct
-	{
-		int m_type;								/**< Tipo de token */
-		char m_name[TOKENIZER_RES_WORD_SIZE];	/**< Identificador de la palabra */
-	}TReservedWord;
-
-	/**
-	 * Encapsula a un bloque representativo de un conjunto especial
-	 * de bloques de texto.
-	 */
-	class CToken
-	{
-	public:
-
-		/**
-		 * Constructor de la clase
-		 * @param p_type Identificador del tipo de token
-		 * @param p_value Valor numérico del token
-		 * @param p_string Cadena de texto token
-		 */
-		CToken(const int p_type = 0, const unsigned int p_value = -1,
-			   char* p_string = NULL);
-		/**
-		 * Destructor de la clase
-		 */
-		virtual ~CToken(){};
-
-		/**
-		 * Establece el tipo del token
-		 * @param p_type Nuevo tipo que se asigna al token
-		 */
-		void setType(const int p_type = 0);
-
-		/**
-		 * Establece el valor numérico del token
-		 * @param p_value Nuevo valor numérico para el token
-		 */
-		void setValue(const unsigned int p_value = -1);
-
-		/**
-		 * Establece el valor textual del token
-		 * @param p_string Nuevo valor textual del token
-		 */
-		void setString(char* p_string = NULL);
-
-		/**
-		 * Devuelve el tipo de token
-		 * @return Identificador del tipo de token
-		 */
-		int getType(void);
-
-		/**
-		 * Devuelve el valor numérico del token
-		 * @return Valor numérico del token
-		 */
-		unsigned int getValue(void);
-
-		/**
-		 * Devuelve el valor textual del token
-		 * @return Puntero al valor textual del token
-		 */
-		char* getString(void);
-
-	private:
-		friend class CTokenizer;
-
-		int m_type;				/**< Identificador del tipo de token */
-		unsigned int m_value;	/**< Valor numérico del token */
-		char* m_string;			/**< Valor textual del token */
-	};
-
 	/**
 	 * Constructor básico de la clase. Se encarga de establecer los valores
 	 * por defecto que utilizará el tokenizador.
 	 */
-	CTokenizer(void);
+	Tokenizer(void);
 
 	/**
-	 * Destructor, se encarga de limpiar la memoria interna del tokenizador
+	 * Constructor parametrizado
+	 * @param delimiters Delimitadores a usar por defecto
+	 * @param detect_str Indica si se deben detectar las cadenas de texto
 	 */
-	~CTokenizer();
+	Tokenizer(const std::string& delimiters, const bool detect_str = true);
+
+	/**
+	 * Destructor
+	 */
+	~Tokenizer();
 
 	/**
 	 * Inicializa el tokenizador sobre un fichero determinado
-	 * @param p_file nombre del fichero con el contenido a tokenizar
+	 * @param file nombre del fichero con el contenido a tokenizar
 	 * @return true si se realizó la carga correctamente
 	 */
-	bool initFromFile(const Glib::ustring& p_file);
+	bool initFromFile(const std::string& file);
 
 	/**
 	 * Inicializa el tokenizador sobre una cadena de texto
-	 * @param p_str Cadena de texto a tokenizar
+	 * @param str Cadena de texto a tokenizar
 	 * @return true si se realizó la carga correctamente
 	 */
-	bool initFromString(const Glib::ustring& p_str);
+	bool initFromString(const std::string& str);
 
 	/**
 	 * Inicializa el tokenizador a partir de un buffer de memoria
-	 * @param p_buffer Puntero al buffer donde se almacenan los datos
-	 * @param p_size Tamaño total del buffer
+	 * @param buffer Puntero al buffer donde se almacenan los datos
+	 * @param size Tamaño total del buffer
 	 * @return true si se realizó la carga correctamente
 	 */
-	bool initFromMemory(const char *p_buffer, int p_size);
+	bool initFromMemory(const char *buffer, const unsigned int size);
 
 	/**
 	 * Establece los delimitadores que debe reconocer el tokenizador para
-	 * separar tokens. Los delimitadores por defecto son ' ', '\t', '\r', '\n'
-	 * @param p_delimiters Array de caracteres delimitadores
-	 * @param p_count Número de caracteres delimitadores en el array
-	 * @note Cuando se llama al método para establecer los delimitadores, los
-	 * delimitadores por defecto son desactivados por lo que cualquiera de ellos
-	 * que deba ser detectado, deberá estár en los nuevos delimitadores.
+	 * separar tokens. Los delimitadores por defecto son ' ', '\n', '\r', '\t',
+	 * '\v', \f'
+	 * @param delimiters Nuevos delimitadores a usar por el tokenizador
+	 * @note El tokenizador continuará por donde estaba, para comenzar de nuevo
+	 * con los nuevos delimitadores, usar reset()
 	 */
-	void setDelimiters(char* p_delimiters, unsigned int p_count);
-
-	/**
-	 * Establece las palabras reservadas que identificará el tokenizador
-	 * @param p_reserved Array de palabras reservadas
-	 * @param p_count Contador de palabras reservadas
-	 */
-	void setReservedWords(TReservedWord p_reserved[], unsigned int p_count);
+	void setDelimiters(const std::string& delimiters);
 
 	/**
 	 * Activa o desactiva la detección de cadenas en el tokenizador.
 	 * Se consideran cadenas las siguientes: "..." ó "...\0
-	 * @param p_detect true para activar la detección, false para desactivarla
-	 * @note Por defecto la detección de cadenas está activada.
+	 * @param detect true para activar la detección, false para desactivarla
 	 */
-	void setStringsDetection(const bool p_detect = true);
-
-	/**
-	 * Activa o desactiva la detección de números en el tokenizador.
-	 * @param p_detect true para activar la detección, false para desactivarla
-	 * @note De momento solo se detectan enteros positivos sin signo
-	 */
-	void setNumbersDetection(const bool p_detect = true);
+	void setStringDetection(const bool detect = true);
 
 	/**
 	 * Obtiene el siguiente token
-	 * @param p_token Token donde dejar el siguiente token
+	 * @return Token leído o "" si no quedan más tokens
 	 */
-	void nextToken(CToken& p_token);
+	std::string nextToken(void);
 
 	/**
 	 * Indica si quedan tokens por leer en el tokenizador
 	 * @return true si quedan más tokens por leer
+	 * @note Posiciona la lectura en el comienzo del siguiente token valido
 	 */
 	bool hasMoreTokens(void);
 
 	/**
-	 * Libera la memoria utilizada por el tokenizador y lo deja como al inicio
+	 * Obtiene el último delimitador encontrado durante el proceso
+	 * @return Carácter delimitador detectado
+	 */
+	char lastDelimiter(void);
+
+	/**
+	 * Resetea el tokenizador para comenzar el proceso de nuevo
 	 */
 	void reset(void);
 
+	/**
+	 * Obtiene todos los tokens restantes de golpe
+	 * @return Vector con los tokens restantes
+	 */
+	std::vector<std::string> split(void);
+
 private:
-	/** Tipo para especificar las palabras reservadas internamente */
-	typedef struct
-	{
-		int m_type;								/**< Tipo de token */
-		int m_size;
-		char m_name[TOKENIZER_RES_WORD_SIZE];	/**< Identificador de la palabra */
-	}TWord;
 
 	/**
-	 * Comprueba si el puntero de lectura del buffer está sobre un delimitador
-	 * @return true si se encontró un delimitador
+	 * Comprueba si la posición de lectura del buffer está sobre un delimitador
+	 * @return true si se encontró un delimitador, false en otro caso
 	 */
 	bool isDelimiter(void);
 
 	/**
-	 * Búsca en las palabras reservadas un token y lo rellena si lo encuentra
-	 * @param p_token Token de destino
-	 */
-	bool checkReservedWords(CToken& p_token);
-
-	/**
-	 * Comprueba si token dado dado es un número
-	 * @param p_token token a comprobar
-	 * @return true si el token dado es un número entero sin signo
-	 */
-	bool checkNum(CToken& p_token);
-
-	/**
-	 * Establece el puntero de lectura del buffer sobre el siguiente token
-	 * @return true si se pudo posicionar, false si se llega al final del texto
-	 */
-	bool findNextToken(void);
-
-	/**
 	 * Obtiene una cadena del buffer del tokenizador
-	 * @return puntero a la cadena
+	 * @return cadena leída
 	 * @note Se consideran cadenas las siguientes: "..." ó "...\0
+	 * @note trata entidades como un solo caracter (\t, \n, etc)
 	 */
-	char* getString(void);
+	std::string getString(void);
 
-	/**
-	 * Obtiene un identificador del buffer del tokenizador
-	 * @return puntero al identificador
-	 */
-	char* getIdentifier(void);
-
-	/**
-	 * Compara dos palabras internas e indica si la primera es menor
-	 * @param p_word0 Primera palabra interna
-	 * @param p_word1 Segunda palabra interna
-	 * @return true si la primera palabra es menor que la segunda
-	 * @note: Este método se utiliza internamente para realizar una ordenación
-	 * de las palabras reservadas.
-	 */
-	static inline bool compareWords(const TWord& p_word0, const TWord& p_word1);
-
-	/**
-	 * Realiza una busqueda binaria en la lista de palabras reservadas
-	 * @param p_key Cadena de texto a buscar en las palabras reservadas
-	 * @return Posición de la palabra reservada o -1 si no se encuentra
-	 */
-	inline int binarySearchWord(char* p_key);
-
-
-	bool m_init;						/**< Indica si el tokenizador se inicio */
-	bool m_detect_strings;				/**< Estado de la detección de cadenas */
-	bool m_detect_numbers;				/**< Estado de la detección de números */
-	char* m_buff;						/**< Buffer del tokenizador */
-	char* m_buff_pos;					/**< Puntero de lectura del buffer */
-	char* m_delimiters;					/**< Lista de delimitadores */
-	unsigned int m_delimiters_count;	/**< Número de delimitadores */
-	char m_default_delimiters[4];		/**< Delimitadores por defecto */
-	TWord* m_words;						/**< Lista de palabras reservadas */
-	unsigned int m_words_count;			/**< Número de palabras reservadas */
-
+	std::string m_buff;						/**< Buffer del tokenizador */
+	std::string::const_iterator m_buff_pos;	/**< Posición de lectura del buffer */
+	std::string m_delimiters;				/**< Lista de delimitadores */
+	std::string m_token;					/**< Token leído */
+	char m_last_delimiter;					/**< Ultimo delimitador encontrado */
+	bool m_detect_strings;					/**< Estado de la detección de cadenas */
 };
 
 #endif // _TOKENIZER_HPP_
