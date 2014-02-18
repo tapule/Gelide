e00c158517ee562a221460b6e6c36576e4556794
Juan Ángel Moreno Fernández <jamf.dev@gmail.com>
2014-02-18 18:38:56 +0100

    Defines updated

--- src/defines.hpp
+++ src/defines.hpp
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
  * gelide
- * Copyright (C) 2008 - 2011 Juan Ángel Moreno Fernández
+ * Copyright (C) 2008 - 2014 Juan Ángel Moreno Fernández
  *
  * gelide is free software.
  *
@@ -19,108 +19,106 @@
  * along with gelide.  If not, see <http://www.gnu.org/licenses/>
  */
 
-#ifndef _GELIDE_HPP_
-#define _GELIDE_HPP_
+#ifndef _DEFINES_HPP_
+#define _DEFINES_HPP_
 
 #ifdef HAVE_CONFIG_H
 	#include <config.h>
-#endif /* HAVE_CONFIG_H */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include "gelide_i18n.hpp"
-
-//#define LOCAL_MODE
-//#define DEBUG_MODE
-
-
-#ifndef HAVE_CONFIG_H
+#else
+	// Defines por defecto
+	#define ENABLE_DEBUG_MODE
 	#define PACKAGE_NAME			"Gelide"
-	#define PACKAGE_VERSION			"0.1.5"
-	#define PACKAGE_CODENAME		 "\"\""
-	#define PACKAGE_WEBSITE			"http://gelide.sourceforge.net"
+	#define PACKAGE_VERSION			"0.2.0"
+	#define PACKAGE_CODENAME		 "\"Theoden\""
+	#define PACKAGE_WEBSITE			"http://gelide.sf.net"
+	#define PACKAGE_DATA_DIR		"."
 #endif
 
-#ifdef DEBUG_MODE
-	#define GELIDE_DEBUG(str) std::cout << "Debug** " << str << std::endl
-	#define GELIDE_WARNING(str) std::cout << "Warning** " << __FILE__ << "[" << __LINE__ << "]: " << str << std::endl
-#else
-	#define GELIDE_DEBUG(str)
-	#define GELIDE_WARNING(str)
+// CHECKME: Este defines solo se utiliza en wip, para release hay que quitarlo
+// Activa el modo de ejecución local.
+// El modo de ejecución local implica que todo el procesamiento se realizará
+// desde el directorio donde reside el binario.
+#define ENABLE_LOCAL_MODE
+
+// En win siempre se usará el modo local
+#ifdef OS_WIN
+	#define ENABLE_LOCAL_MODE
+#endif
+
+#ifndef ENABLE_DEBUG_MODE
 	#define NDEBUG
 #endif
 
-#define GELIDE_ERROR(str) std::cerr << "Error** " << __FILE__ << "[" << __LINE__ << "]: " << str << std::endl; exit(1)
-
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 #include <cassert>
+#include "utils/i18n.hpp"
+#include "utils/os_detect.hpp"
+#include "utils/debug.hpp"
+#include "utils/log.hpp"
 
-#ifndef LOCAL_MODE
-	#define GELIDE_ICON				PACKAGE_DATA_DIR"/icons/hicolor/scalable/apps/gelide.svg"
-	#define GELIDE_PIXMAPS			PACKAGE_DATA_DIR"/gelide/pixmaps/"
-	#define GELIDE_UI_ICONS			PACKAGE_DATA_DIR"/gelide/ui/icons/"
-	#define GELIDE_CFG_DEFAULT		PACKAGE_DATA_DIR"/gelide/defaultcfg.xml"
-	#define GELIDE_DOCS				PACKAGE_DOC_DIR"/gelide/"
+
+// Directorios de trabajo en función del tipo de ejecución. La versión win
+// siempre se compilará con modo local activado.
+#ifdef ENABLE_LOCAL_MODE
+	#ifdef OS_POSIX
+		#define GELIDE_DATA_DIR				"data"
+		#define GELIDE_ICON_DIR				GELIDE_DATA_DIR"/icontheme"
+		#define GELIDE_ICONTHEME_DIR		GELIDE_DATA_DIR"/icontheme"
+		#define GELIDE_COLLECTIONS_ICONS_DIR	GELIDE_DATA_DIR"/icontheme/collections"
+		#define GELIDE_CHANGELOG_DIR		"."
+		#define GELIDE_HELP_DIR				"help"
+	#else
+		#define GELIDE_DATA_DIR				"data"
+		#define GELIDE_ICON_DIR				GELIDE_DATA_DIR"\\icontheme"
+		#define GELIDE_ICONTHEME_DIR		GELIDE_DATA_DIR"\\icontheme"
+		#define GELIDE_COLLECTIONS_ICONS_DIR	GELIDE_DATA_DIR"\\icontheme\\collections"
+		#define GELIDE_CHANGELOG_DIR		"."
+		#define GELIDE_HELP_DIR				"help"
+	#endif
 #else
-	#define GELIDE_ICON				"../data/gelide.svg"
-	#define GELIDE_PIXMAPS			"../pixmap/"
-	#define GELIDE_UI_ICONS			"../ui/icons/"
-	#define GELIDE_CFG_DEFAULT		"../data/defaultcfg.xml"
-	#define GELIDE_DOCS				"../"
+	#define GELIDE_DATA_DIR					PACKAGE_DATA_DIR"/gelide"
+	#define GELIDE_ICON_DIR					PACKAGE_DATA_DIR"/icons/hicolor/scalable/apps"
+	#define GELIDE_ICONTHEME_DIR			GELIDE_DATA_DIR"/icontheme"
+	#define GELIDE_COLLECTIONS_ICONS_DIR	GELIDE_DATA_DIR"/icontheme/collections"
+	#define GELIDE_CHANGELOG_DIR			GELIDE_DATA_DIR
+	#define GELIDE_HELP						"help" //?????
 #endif
 
+// Estructura de directorios para datos del usuario
+// Toda la estructura se montará a partir del directory de trabajo del usuario
+#define USER_LIBRARY_DIR				"library"
+#define USER_COLLECTIONS_DIR			"collections"	// library/collections
 
-#define GELIDE_DIR					".gelide"
-#define GELIDE_CHANGELOG			GELIDE_DOCS"ChangeLog"
-#define USER_SYSTEMS_LIST			"systems.gsl"
+// Estructura de directorios para las colecciones
+// Toda la estructura se montará a partir del directorio de la colección
+#define COLLECTION_ROMS_DIR				"roms"
 
-// Fichero de configuración y grupos en el fichero de configuración
-#define GELIDE_CFG_FILE				"gelidecfg.xml"
-#define GELIDE_CFG_DIALOGS			"dialogs"
-#define GELIDE_CFG_PREFERENCES		"preferences"
-#define GELIDE_CFG_UI				"ui"
+// Directorio de trabajo por defecto
+#define GELIDE_DEFAULT_DIR				".gelidewip"
 
-#define SYSTEMS_IMAGES				"images"
-#define SYSTEMS_IMAGES_SNAP			SYSTEMS_IMAGES"/snap"
-#define SYSTEMS_IMAGES_TITLE		SYSTEMS_IMAGES"/title"
-#define SYSTEMS_IMAGES_FRONT		SYSTEMS_IMAGES"/front"
-#define SYSTEMS_IMAGES_BACK			SYSTEMS_IMAGES"/back"
-#define SYSTEMS_IMAGES_MEDIA		SYSTEMS_IMAGES"/media"
-#define SYSTEMS_ROMS				"roms"
-#define SYSTEMS_GAMES_LIST			"games.ggl"
-#define SYSTEMS_EMULATORS_LIST		"emulators.gel"
-#define SYSTEMS_EXPORT_ICONS_DIR	"icons"
-#define SYSTEMS_EXPORT_DATS_DIR		"dats"
+// Ficheros manejados por Gelide
+#ifdef OS_POSIX
+	#define GELIDE_ICON_FILE			"gelide.svg"
+#else
+	#define GELIDE_ICON_FILE			"gelide.png"
+#endif
+#define GELIDE_DB_FILE					"gelide.sqlite"
+#define GELIDE_CFG_FILE					"gelide.xml"
+#define GELIDE_LOG_FILE					"gelide.log"
+#define GELIDE_LASTRUN_LOG_FILE			"lastrun.log"
+#define GELIDE_CHANGELOG_FILE			"ChangeLog"
 
+// Ficheros manejados por el metadbmanager
+#define METADB_DB_FILE					"metadb.sqlite"
 
-#define GELIDE_VERSION				PACKAGE_VERSION
-#define GELIDE_WEBSITE				PACKAGE_WEBSITE
-#define GELIDE_WIDTH				1500
-#define GELIDE_HEIGHT				800
-
-#define FILTER_TYPE_NOFILTER		-1
-#define FILTER_TYPE_ALL				0
-#define FILTER_TYPE_FAVORITE		1
-#define FILTER_TYPE_PLAYED			2
-#define FILTER_TYPE_WORKING			3
-#define FILTER_TYPE_AVAILABLE		4
-#define FILTER_TYPE_UNKNOWN			5
-#define FILTER_TYPE_RANK			6
-#define FILTER_TYPE_YEAR			7
-
-#define CSV_SEPARATOR				"\x3B"	// ";"
-
-#define PARAMS_TAG_SIZE				3
-#define PARAMS_TAG_GAME_NAME		"$gn"		// Nombre del set
-#define PARAMS_TAG_GAME_FILE		"$gf"		// Path + fichero del juego
-#define PARAMS_TAG_GAME_PATH		"$gp"		// Path del juego
-#define PARAMS_TAG_BIOS_FILE		"$bf"		// Path + fichero bios
-#define PARAMS_TAG_BIOS_PATH		"$bp"		// Path del fichero bios
-#define PARAMS_TAG_EMULATOR_FILE	"$ef"		// Path + binario del emulador
-#define PARAMS_TAG_EMULATOR_PATH	"$ep"		// Path del binario del emulador
-/* ESTA ETIQUETA SE ELIMINARÁ EN LA PRÓXIMA VERSIÓN */
-#define PARAMS_TAG_ROMS_PATH		"$rp"
+// Root, versión y grupos para el fichero de configuración de Gelide
+#define GELIDE_CFG_ROOT					"gelidecfg"
+#define GELIDE_CFG_VERSION				"0.2"
+#define GELIDE_CFG_GLOBAL				"global"
+#define GELIDE_CFG_UI					"ui"
+#define GELIDE_CFG_DIALOGS				"dialogs"
 
 
-
-#endif // _GELIDE_HPP_
+#endif // _DEFINES_HPP_
